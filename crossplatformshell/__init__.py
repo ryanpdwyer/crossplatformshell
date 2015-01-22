7ac02643f47056d399a5d5f1068c005a62416392
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-22 07:53:57 -0500

    A few tests for the shell commands

--- crossplatformshell/__init__.py
+++ crossplatformshell/__init__.py
@@ -4,8 +4,66 @@
 crossplatformshell
 ============================
 """
+from __future__ import (print_function, division, absolute_import,
+                       unicode_literals)
+
+import pathlib
+import io
+import os
+import shutil
+import distutils.dir_util
+
+# Use subprocess32 if available
+try:
+    import subprocess32 as subprocess
+except:
+    import subprocess as subprocess
 
 # Versioneer versioning
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+def new_path(path_string):
+    """Return pathlib.Path, expanding '~' to a user's HOME directory"""
+    return pathlib.Path(os.path.expanduser(path_string))
+
+
+def copy(src_path, dst_path):
+    shutil.copy(str(src_path), str(dst_path))
+
+cp = copy
+
+
+def copy_tree(src_path, dst_path):
+    """Recursively copy all files and folders from src_path to dst_path"""
+    distutils.dir_util.copy_tree(str(src_path), str(dst_path))
+
+cp_r = copy_tree
+
+def mkdir(path):
+    """Make a directory from the specified path"""
+    os.mkdir(str(path))
+
+
+def remove(path):
+    """Remove the specified path."""
+    os.remove(str(path))
+
+rm = remove
+
+def rmtree(path):
+    """Recursively remove paths."""
+    shutil.rmtree(str(path))
+
+rm_rf = rmtree
+
+def read_file(filename):
+    return io.open(str(filename)).read()
+
+
+def write_file(filename, string):
+    io.open(str(filename), 'w', encoding="utf-8").write(string)
+
+
--- crossplatformshell/tests/test_crossplatformshell.py
+++ crossplatformshell/tests/test_crossplatformshell.py
@@ -2,17 +2,45 @@
 """
 Tests for `crossplatformshell` module.
 """
+from __future__ import (print_function, division, absolute_import,
+                       unicode_literals)
+
 import unittest
-import crossplatformshell
+import os
+import tempfile
+
+from crossplatformshell import (new_path, copy, copy_tree, mkdir, remove,
+                                rmtree, write_file)
 
 
 class TestCrossplatformshell(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    fname = 'tempfile.txt'
 
-    def test_something(self):
-        pass
+    def setUp(self):
+        self.dir = new_path(tempfile.mkdtemp())
+        self.file = self.dir/self.fname
+        write_file(self.file, "Test file contents\n")
+
+    def test_copy(self):
+        copied_file = 'tempfile_copy.txt'
+        copy(self.file, self.dir/copied_file)
+        self.assertTrue(os.path.isfile(str(self.dir/copied_file)))
+
+    def test_mkdir(self):
+        new_dir = 'new_dir'
+        mkdir(self.dir/new_dir)
+        self.assertTrue((self.dir/new_dir).is_dir())
+
+    def test_copy_tree(self):
+        new_dir = 'new_dir'
+        cp_dir = 'copied_dir'
+        new_file = 'testfile.txt'
+        mkdir(self.dir/new_dir)
+        write_file(self.dir/new_dir/new_file, "Test file contents\n")
+        copy_tree(self.dir/new_dir, self.dir/cp_dir)
+        self.assertTrue((self.dir/new_dir).is_dir())
+        self.assertTrue((self.dir/new_dir/new_file).is_file())
 
     def tearDown(self):
-        pass
+        rmtree(self.dir)
